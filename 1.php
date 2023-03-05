39eb4f699e039acb470c3894bc7c0e43b50a225a
Reihaneh <104936298+reihaneh-b60@users.noreply.github.com>
2023-03-05 11:10:11 +0330

    first commit

---
+++ 1.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="2.php" method="post">
+    <td>کد</td>
+<input type="number" name="cod">
+<br>
+<br>
+<td>فامیل</td>
+<input type="text" name="family">
+<br>
+<br>
+<td>خودکار</td>
+<input type="text" name="pen">
+<br>
+<br>
+<td>رنگ</td>
+<input type="text" name="book">
+<br>
+<br>
+<td>مداد</td>
+<input type="text" name="cod">
+<br>
+<br>
+<button type="submit" name="show">نمایش</button>
+<button type="submit" name="add">درج</button>
+</form>
+
+</body>
+</html>
